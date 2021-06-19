c63ea4fc769f592c87af40235af1d43bd8ce0b4c
rahulprasad2000 <rahulprasad4262@gmail.com>
2021-06-20 01:56:44 +0530

    updated basic files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, jsonify, url_for, redirect, flash, Response, send_file
 from camera import VideoCamera
+import os 
+from flask import send_from_directory 
+import cv2    
 
 
 app = Flask(__name__)
+
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 
 @app.route('/')
 def index():
@@ -12,12 +22,15 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + bytearray(frame) + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
-    mimetype='multipart/x-mixed-replace; boundary=frame')
+           mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host=os.getenv('HOST', '127.0.0.1'), port=os.getenv('PORT', 5000), debug=app.config.get('FLASK_DEBUG'))
+   
+    
+    
